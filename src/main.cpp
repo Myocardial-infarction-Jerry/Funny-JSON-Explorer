9270f5fed340e621a1c517221b746df5e5b0382f
NTSCoS <liusy228@mail2.sysu.edu.cn>
2024-05-30 15:56:49 +0800

    🎉 init(frame): Set main frame

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <argparse/argparse.hpp>
+#include <nlohmann/json.hpp>
+
+#include "FunnyJSONExplorer.hpp"
+
+using ArgumentParser = argparse::ArgumentParser;
+using json = nlohmann::json;
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
